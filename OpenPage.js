8dd02aa0e09f583d6ea094a0e45c66b0c9aa276e
NorthJaw <northjaw@yahoo.co.in>
2017-02-01 20:49:25 +0530

    Add files via upload

--- OpenPage.js
+++ OpenPage.js
@@ -35,7 +35,7 @@ $('.Button').click(function(event){
 });
 
 
-$(':input').focusin(function(){
+$('.LoginField').focusin(function(){
 	
 	$(this).css('background-color','#F9E79F');
 }).focusout(function(){
